2144c8f677a984c950617c2fbe05a5d19f2afb67
SudhansuSJena <sudhansus.jena@hyscaler.com>
2024-11-19 18:01:48 +0530

    New View added

--- addons/om_hospital/__manifest__.py
+++ addons/om_hospital/__manifest__.py
@@ -3,9 +3,13 @@
   "author": "SUDHANSU SEKHAR JENA",
   "license": "LGPL-3",
   "version": "17.0.1.1",
+  "depends": [
+    "mail"  
+  ],
   "data": [
       "security/ir.model.access.csv",
       "views/om_hospital_patient_views.xml",
+      "views/om_hospital_patient_readonly_views.xml",
       "views/om_hospital_menu.xml"
   ],
   "application": "True", # Add this to make it appear in apps list
--- addons/om_hospital/models/om_hospital_patient.py
+++ addons/om_hospital/models/om_hospital_patient.py
@@ -3,8 +3,9 @@ from odoo import api, fields, models
 class OmHospitalPatient(models.Model):
     _name = "om_hospital.patient"
     _description = "Patient Master"
+    _inherit = ["mail.thread"]
 
-    name = fields.Char(string="Name", required=True)
-    date_of_birth = fields.Date(string="Date of birth")
+    name = fields.Char(string="Name", required=True, tracking=True)
+    date_of_birth = fields.Date(string="Date of birth", tracking=True)
 
-    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
+    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender", tracking=True)
